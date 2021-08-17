f0ee0ad4581403ba393106cd187b4455e25cfabe
shashidharnaik55 <shashidharnaik55@gmail.com>
2021-08-17 13:55:54 +0530

    call back hell task

---
+++ script.js
@@ -0,0 +1,36 @@
+setTimeout(() => {
+  document.querySelector('#counter').innerHTML = '10';
+  setTimeout(() => {
+    document.querySelector('#counter').innerHTML = '9';
+    setTimeout(() => {
+      document.querySelector('#counter').innerHTML = '8';
+      setTimeout(() => {
+        document.querySelector('#counter').innerHTML = '7';
+        setTimeout(() => {
+          document.querySelector('#counter').innerHTML = '6';
+          setTimeout(() => {
+            document.querySelector('#counter').innerHTML = '5';
+            setTimeout(() => {
+              document.querySelector('#counter').innerHTML = '4';
+              setTimeout(() => {
+                document.querySelector('#counter').innerHTML = '3';
+                setTimeout(() => {
+                  document.querySelector('#counter').innerHTML = '2';
+                  setTimeout(() => {
+                    document.querySelector('#counter').innerHTML = '1';
+                    setTimeout(() => {
+                      document.querySelector('#counter').innerHTML =
+                        'Happy Independence Day';
+                      document.querySelector('#disply-image').style.visibility =
+                        'visible';
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
